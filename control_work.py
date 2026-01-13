96946cfbc79a8812a84b309cc13a9d0a378c9e18
Somerkom <aovdtls@gmail.com>
2026-01-13 18:52:06 +0600

    добавила сеттеры

--- control_work.py
+++ control_work.py
@@ -7,9 +7,19 @@ class Animal:
     @property
     def name(self):
         return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
     @property
     def age(self):
         return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        self.__age = new_age
+
 
     def make_sound(self):
         pass
@@ -20,13 +30,14 @@ class Cat(Animal):
     def make_sound(self):
         print("Мияу мияу")
 
-cat = Cat("Алиса","2 месяца")
+cat = Cat("Алиса","2")
 cat.make_sound()
+cat.name = "Лиса"
+cat.age = "2"
+print(f"{cat.name}, {cat.age} года")
 print("*" * 35)
-dog = Dog("Пупи","3месяца")
+dog = Dog("Пупи","3")
 dog.make_sound()
-
-
-"""
-
-"""
+dog.name = "Муся"
+dog.age = "4"
+print(f"{dog.name}, {dog.age} года")
